a4c007b1efca74eaea31766634c99af3853b8da5
masudcse100 <masudcse74@gmail.com>
2022-11-05 00:37:15 +0600

    Call Apply

---
+++ call-apply.js
@@ -0,0 +1,16 @@
+const normalPerson = {
+  fname: 'Rahim',
+  lname: 'Mia',
+  salary: 20000,
+  getFullName: function(){
+    console.log(this.fname, this.lname);
+  },
+  chargeBill: function(ammount){
+    this.salary = this.salary - ammount;
+    return this.salary;
+  }
+}
+// console.log(normalPerson);
+normalPerson.chargeBill(550);
+normalPerson.chargeBill(2500);
+console.log(normalPerson.salary);
